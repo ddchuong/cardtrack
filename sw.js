be0686c54edf46a566ddd91ef2153b8714f9d606
ddchuong <dinhduychuong@gmail.com>
2026-08-05 15:27:11 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,32 @@
-const CACHE_NAME = 'cardtrack-v1';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+const CACHE_NAME='cardtrack-v1';
+const APP_SHELL=['./','./index.html'];
+self.addEventListener('install',(event)=>{
+  event.waitUntil((async()=>{
+    const cache=await caches.open(CACHE_NAME);
+    await Promise.all(APP_SHELL.map((u)=>cache.add(u).catch(()=>{})));
+  })());
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate',(event)=>{
+  event.waitUntil(caches.keys().then((keys)=>Promise.all(
+    keys.filter((k)=>k!==CACHE_NAME).map((k)=>caches.delete(k))
+  )));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch',(event)=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(
+    caches.match(event.request).then((cached)=>{
+      const network=fetch(event.request).then((res)=>{
+        if(res && res.status===200){
+          const copy=res.clone();
+          caches.open(CACHE_NAME).then((c)=>c.put(event.request,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || network;
+    })
   );
 });
