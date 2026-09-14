5fd558cacb3d07723a76426a8c8b40c22af2be36
ddchuong <dinhduychuong@gmail.com>
2026-09-14 11:32:51 +0700

    deploy 202609141132

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy so old caches get purged and clients pick up new code.
-const CACHE_NAME='cardtrack-v202609141114';
+const CACHE_NAME='cardtrack-v202609141132';
 const APP_SHELL=['./','./index.html'];
 
 self.addEventListener('install',function(e){
